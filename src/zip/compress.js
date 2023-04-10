80e975eea534c7f8f476e4a6bda00e4889938943
Rustam_Satlykov <rustam.satlykov@syngenta.com>
2023-04-10 23:15:27 +0500

    The "Zlib" subtasks were completed.

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,15 @@
+import fs from 'node:fs';
+import path from 'path';
+import zlib from 'zlib';
+
+const filesPathArchive = path.resolve('src/zip/files/archive.gz');
+const filesPathToCompress = path.resolve('src/zip/files/fileToCompress.txt');
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip();
+  const readStream = fs.createReadStream(filesPathToCompress);
+  const writeStream = fs.createWriteStream(filesPathArchive);
+  readStream.pipe(gzip).pipe(writeStream);
 };
 
 await compress();
