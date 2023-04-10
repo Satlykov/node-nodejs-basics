4476f9542c15f0074eee49ae336c22669a8ccd02
Rustam_Satlykov <rustam.satlykov@syngenta.com>
2023-04-10 22:43:13 +0500

    The "Streams" subtasks were completed.

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,17 @@
+import stream from 'stream';
+
+class ReverseTransform extends stream.Transform {
+  _transform(data, _, callback) {
+    this.push(data.toString().split('').reverse().join(''));
+    callback();
+  }
+}
+
 const transform = async () => {
-    // Write your code here 
+  let readStream = process.stdin;
+  let writeStream = process.stdout;
+  let transformStream = new ReverseTransform();
+  readStream.pipe(transformStream).pipe(writeStream);
 };
 
 await transform();
