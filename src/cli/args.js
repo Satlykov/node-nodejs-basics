b086a3259b998e72c213feebc8ee485836e21645
Rustam_Satlykov <rustam.satlykov@syngenta.com>
2023-04-10 21:10:42 +0500

    The "Command line interface(src/cli)" subtasks were completed.

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv.slice(2);
+
+  const mapArgs = args
+    .map((str) => (str.startsWith('--') ? str.slice(2) + ' is ' : str))
+    .map((el, i) => (i % 2 !== 0 && i !== args.length - 1 ? el + ', ' : el));
+
+  console.log(mapArgs.join(''));
 };
 
 parseArgs();
--- src/cli/env.js
+++ src/cli/env.js
@@ -1,6 +1,11 @@
 const parseEnv = () => {
-  const args = process.env;
-  console.log(args);
+  const params = [];
+  for (const param in process.env) {
+    param.startsWith('RSS_')
+      ? params.push(`${param}=${process.env[param]}`)
+      : null;
+  }
+  console.log(params.join('; '));
 };
 
 parseEnv();
